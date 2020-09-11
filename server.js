32550f7136736deab5712de8cb0e1688f684dbdd
afghl <airport.0930@gmail.com>
2020-09-11 21:54:32 +0800

    add server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+
+app.get('/apis/playlist', (req, res) => {
+  res.send(req.query.playlistId)
+})
+
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
