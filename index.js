37a5059c7bdd3accc9775f587cc21b8bb1ff0d7c
afghl <airport.0930@gmail.com>
2020-09-11 21:30:25 +0800

    try to request api

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 #!/usr/bin/env node
 
-const parseArgs = require('minimist')
+const parseArgs = require('minimist');
+const request = require('request');
+
+const YT_PLAYLIST_FORMAT = /^([\S]+list=)?([\w_-]+)[\S]*$/;
+const PLAYLIST_ITEM_URL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&fields=items/contentDetails/videoId,nextPageToken&key=AIzaSyAhMj9pBxRpzUlD8elg-ckIclhY_5ngEBk&playlistId={playlistId}&pageToken={pageToken}';
+
+start()
+
+const getVideoIds = (playlistId) => {
+  let videoIds = [];
+  let pageToken = ""
+  while (true) {
+    const url = PLAYLIST_ITEM_URL.replace("{playlistId}", playlistId).replace("{pageToken}", pageToken);
+
+    request('http://www.google.com', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        const data = JSON.parse(body);
+        console.log(data);
+      }
+    });
+
+
+    break;
+  }
+}
 
 const start = () => {
 
   const args = parseArgs(process.argv.slice(2));
-  console.log(args);
-  if (!args) {
-    console.log("youtube playlist url is required.");
+
+  if (args == null) {
+    console.log("Youtube playlist url is required.");
     return;
   }
 
-  if (true) {
+  const youtubeUrl = args['_'][0];
 
+  if (youtubeUrl == null || youtubeUrl == undefined) {
+    console.log("Youtube playlist url is required.");
+    return;
   }
-}
 
-console.log("Hello world2");
-start()
+  const playlistId = youtubeUrl.match(YT_PLAYLIST_FORMAT)[2];
+
+  if (playlistId == null) {
+    console.log("Invalid youtube playlist url.");
+  }
+
+  const videoIds = getVideoIds(playlistId)
+}
